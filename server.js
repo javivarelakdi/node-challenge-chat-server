fe899671efa663145afc9f22a6384bf931b8001c
Javier Varela <jvarela@strands.com>
2020-11-20 22:21:36 +0100

    crud

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const cors = require("cors");
-
+require("dotenv").config();
+// const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
-
-app.use(cors());
+// app.use(cors());
+app.use(bodyParser.json()); 
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const welcomeMessage = {
-  id: 0,
+  id: 1,
   from: "Bart",
-  text: "Welcome to CYF chat system!",
+  text: "Welcome to CYF chat system!"
 };
 
 //This array is our "data store".
@@ -20,4 +22,32 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+app.get("/messages/:id", function (req, res) {
+  const message = messages.filter(item => item.id === parseInt(req.params.id));
+  res.send(message);
+});
+
+app.get("/messages", function (req, res) {
+  res.send(messages);
+});
+
+app.post("/messages", urlencodedParser, function (req, res) {
+  const newId = (messages[messages.length - 1].id) +1;
+  const newMessage = {
+    id: newId,
+    from: req.body.from,
+    text: req.body.text
+  }
+  messages.push(newMessage)
+  res.send(messages);
+});
+
+app.delete("/messages/:id", function (req, res) {
+  const index = messages.indexOf(messages.find(item => item.id === req.params.id));
+  if (index > -1) {
+    messages.splice(index, 1);
+  }
+  res.send(messages)
+});
+
+app.listen(process.env.PORT || 3000);
